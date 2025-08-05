df114d6a4d76ef6e534b58c4b258ab8061ab3507
sj-minRva <rheamaria13@gmail.com>
2025-08-05 18:25:56 +0530

    Basics Practice

--- Easy/Basics of Python.py
+++ Easy/Basics of Python.py
@@ -30,8 +30,9 @@ else:
 '''
 
 #List
-
+"""
 names = ["Mary", "Sid", "Sam", "Max", "Alex", "Eleven", "Mick"] #list
+
 #slicing of strings
 print(names[-2])
 print(names[0:3:])
@@ -40,3 +41,72 @@ print(names[0:6:2])
 print("Before change: ", names)
 names[0] = "John"
 print("After change: ", names)
+"""
+
+#Opertion on List
+'''
+numbers = [1, 3, 4, 7, 6,34, 22, 1, 3, 9]
+numbers.append("rhea")
+print(numbers)
+
+numbers.insert(0, 0)
+print(numbers)
+
+numbers.remove('rhea')
+print(numbers)
+
+numbers.pop()  # removes the last element
+print(numbers)
+
+numbers.sort()  # sorts the list
+print(numbers)
+
+numbers.reverse()  # reverses the list
+print(numbers)
+
+numbers.clear()  # clears the list
+print(numbers)
+'''
+
+#Range
+
+numbers = range(5,20,3)
+print(numbers)
+
+for i in range(5,20,2):
+    print(i)
+
+#Tuple
+numbers = (1,2,3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
