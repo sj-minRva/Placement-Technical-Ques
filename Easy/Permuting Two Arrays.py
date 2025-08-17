7aecd3bce9adf76ff5fa683e7f8d3d378fc54af7
sj-minRva <rheamaria13@gmail.com>
2025-08-17 09:26:28 +0530

    Hacker Rank w1

--- Easy/Permuting Two Arrays.py
+++ Easy/Permuting Two Arrays.py
@@ -2,14 +2,11 @@
 def twoArrays(k, A, B):
     A.sort()
     B.sort(reverse = True)
-    print(A)
-    print(B)
     
     for x in range(len(A)):           
         if A[x]+B[x] < k:
             return 'NO'
-        else:
-            return 'YES'
+    return 'YES'
 
 A = [1,0,0,1,1,1]
 B = [3,1,2,1,1,1]
