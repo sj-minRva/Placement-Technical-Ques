a62cf9b942dca5aec699d5b1c70bdf0acedb8857
sj-minRva <rheamaria13@gmail.com>
2025-08-29 23:02:16 +0530

    basics

--- Easy/Practice/How classes work.py
+++ Easy/Practice/How classes work.py
@@ -4,3 +4,20 @@ class Example:
 
 obj = Example()
 obj.say_hello("Rhea")
+
+
+class Counter:
+    
+    def increment(c1):
+        c1 += 1   # needs self to refer to THIS object's count
+
+c1 = 0
+c2 = 1
+
+c1.increment(c1)
+c1.increment(c1)
+c2.increment(c2)
+
+print(c1.count)  
+print(c2.count)  
+
