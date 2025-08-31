da75702b4e931e38a53c9dc8d4658feba58f9b08
sj-minRva <rheamaria13@gmail.com>
2025-08-31 21:02:24 +0530

    Remove Element - leetcode

--- Easy/Remove Elements - leetcode.py
+++ Easy/Remove Elements - leetcode.py
@@ -1,5 +1,14 @@
-class Solution:
-    def removeElement(self, nums, val):
 
-        b
+def removeElement(nums, val):
+    k = 0
+    length = len(nums)
+    for i in range(len(nums)):        
+        if nums[i] != val:
+            k+=1
+
+    return k
+
+nums = [0,1,2,2,3,0,4,2]
+val = 2
+removeElement(nums, val)
         
