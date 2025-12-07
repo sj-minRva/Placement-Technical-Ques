261354ba11f54c3b88a603babb71cadaa0bb20b4
sj-minRva <rheamaria13@gmail.com>
2025-12-07 14:32:42 +0530

    Prefix Sum problem in java

---
+++ Important Programs/Prefix Sum/Prefix.java
@@ -0,0 +1,11 @@
+public int[] prefixSum(int[] arr) {
+    int n = arr.length;
+    int[] prefix = new int[n];
+    
+    prefix[0] = arr[0];
+    for (int i = 1; i < n; i++) {
+        prefix[i] = prefix[i - 1] + arr[i];
+    }
+    
+    return prefix;
+}
