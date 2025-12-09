f0fd4cddc04d9d7781070b2cc7c17a71d9c003a7
sj-minRva <rheamaria13@gmail.com>
2025-12-09 07:34:45 +0530

    prefix sum

--- Important Programs/Prefix Sum/Prefix.java
+++ Important Programs/Prefix Sum/Prefix.java
@@ -1,11 +1,22 @@
-public int[] prefixSum(int[] arr) {
-    int n = arr.length;
-    int[] prefix = new int[n];
-    
-    prefix[0] = arr[0];
-    for (int i = 1; i < n; i++) {
-        prefix[i] = prefix[i - 1] + arr[i];
+public class Prefix {
+    public int[] prefixSum(int[] arr) {
+        int n = arr.length;
+        int[] prefix = new int[n];
+        
+        prefix[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+        
+        return prefix;
     }
     
-    return prefix;
+    public static void main(String[] args) {
+        Prefix p = new Prefix();
+        int[] arr = {1, 2, 3, 4, 5};
+        int[] result = p.prefixSum(arr);
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
 }
--- Important Programs/Prefix Sum/Prefix.py
+++ Important Programs/Prefix Sum/Prefix.py
@@ -1,5 +1,5 @@
 def prefix_sum(arr):
-    prefix = [0] * len(arr)
+    prefix = [] 
     prefix[0] = arr[0]
     for i in range(1, len(arr)):
         prefix[i] = prefix[i-1] + arr[i]
